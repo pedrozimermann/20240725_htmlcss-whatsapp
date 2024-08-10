94ea4c1c758d5b961c8f87402d1baae2f10cee83
Pedro Zimermann <phzpaiva@gmail.com>
2024-08-09 22:05:43 -0300

    Update

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 $(function (e) {
-    var $messageInput = $(' .main-container .mc-right footer input');
+    var $messageInput = $(' .main-container .mc-right footer input'),
+        $actionSend = $('.icon-action-send'),
+        messageTemplate = `
+    <div class="message">
+      <span data-time="_time">_message</span>
+    </div>`;
+
+    $actionSend.on('click', function (e) {
+        var $this = $(this),
+            myMessage = messageTemplate,
+            $mt,
+            value = $messageInput.val();
+
+            if(value.length > 0){
+        myMessage = myMessage
+            .replace('_message', value)
+            .replace('_time', '21:53');
+
+        $mt = $(myMessage);
+        $mt.addClass('message-right');
+
+        $('.messages-container').append($mt);
+    }
+
+    });
 
     $messageInput.on('keyup', function (e) {
         var $this = $(this),
             value = $this.val(),
-            valueLen = value.length,
-            $actionSend = $('.icon-action-send');
+            valueLen = value.length
 
         if (valueLen > 0) {
             $actionSend
@@ -13,9 +36,11 @@ $(function (e) {
                 .addClass('fa-share');
         } else {
             $actionSend
-            .removeClass('fa-share')
-            .addClass('fa-microphone');
+                .removeClass('fa-share')
+                .addClass('fa-microphone');
         }
 
     });
+
+
 });
